6b19736a8f4a3637353a6120d8283821a450baa6
shawn <blueshieldcross@gmail.com>
2019-07-31 09:05:37 -0400

    seeding data

--- LibraryManagementSystem/Global.asax.cs
+++ LibraryManagementSystem/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using LibraryManagementSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,10 +14,9 @@ namespace LibraryManagementSystem
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new LibraryDBInitializer());
+
             AreaRegistration.RegisterAllAreas();
-            UnityConfig.RegisterComponents();
-
-
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
